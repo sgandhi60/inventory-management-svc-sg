d5b3b1d916b91f3518c1cb9fc83a2c03bbf38ee1
Shailesh Gandhi <shailesh@Shaileshs-MacBook-Pro.local>
2020-08-31 15:59:33 -0400

    Adds Application and Removes default Application class

--- src/main/java/com/ibm/inventory_management/app/Application.java
+++ src/main/java/com/ibm/inventory_management/app/Application.java
@@ -1,8 +1,6 @@
-package application.app;
+package com.ibm.inventory_management.app;
 
-import io.jaegertracing.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,15 +12,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-@ComponentScan({"com.ibm.hello.*", "com.ibm.cloud_garage.*", "com.ibm.health"})
+@ComponentScan({"com.ibm.inventory_management.*", "com.ibm.cloud_garage.*", "com.ibm.health"})
 public class Application extends SpringBootServletInitializer {
     @Autowired
     Environment environment;
-    @Value("${spring.application.name}")
-    private String applicationName;
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class);
+        SpringApplication.run(com.ibm.inventory_management.app.Application.class, args);
     }
 
     @Bean
@@ -38,18 +34,5 @@ public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Application.class);
-    }
-
-    @Bean
-    public io.opentracing.Tracer initTracer() {
-        Configuration.SamplerConfiguration samplerConfig = new Configuration.SamplerConfiguration()
-                .withType("const")
-                .withParam(1);
-        Configuration.ReporterConfiguration reporterConfig = Configuration.ReporterConfiguration.fromEnv()
-                .withLogSpans(true);
-
-        return Configuration.fromEnv(this.applicationName)
-                .withSampler(samplerConfig)
-                .withReporter(reporterConfig).getTracer();
     }
 }
